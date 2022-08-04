7e8a72f99dff7b9f68f3f77409b271a16ca8cc86
Apelsini <apelsin652@gmail.com>
2022-08-04 12:16:29 +0300

    hello world check

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,7 +5,7 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'It works!\n'
+    message = 'Hello World!\n'
     version = 'Python %s\n' % sys.version.split()[0]
     response = '\n'.join([message, version])
     return [response.encode()]
