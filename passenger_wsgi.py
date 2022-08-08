d19da7fec8454bb80217d289ce5e20d0863240a1
Apelsini <apelsin652@gmail.com>
2022-08-08 17:37:41 +0300

    import and use selenium to get URL

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,15 @@
 import os
 import sys
 import requests
+from selenium import webdriver
 
 sys.path.insert(0, os.path.dirname(__file__))
+driver = webdriver.Firefox()
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     message = 'Hello people!\n'
     version = 'Python %s\n' % sys.version.split()[0]
-    resp = sys.path.__str__().__str__()
+    resp = driver.current_url
     response = '\n'.join([message, version, resp])
     return [response.encode()]
