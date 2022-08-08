a0658e3cc05902da884cf22eab5d5ecedb96dbc2
Apelsini <apelsin652@gmail.com>
2022-08-08 18:00:02 +0300

    splinter browser

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import requests
+from splinter import browser
 
 sys.path.insert(0, os.path.dirname(__file__))
 
@@ -8,6 +9,6 @@ def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     message = 'Hello people!\n'
     version = 'Python %s\n' % sys.version.split()[0]
-    resp = 0
+    resp = browser.url
     response = '\n'.join([message, version, resp])
     return [response.encode()]
