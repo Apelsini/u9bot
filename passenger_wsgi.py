ceaa6028ba1b836ea94887274eeae5bd5d029dee
Apelsini <apelsin652@gmail.com>
2022-08-08 17:42:15 +0300

    OS_environ  getting URL

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,15 +1,13 @@
 import os
 import sys
 import requests
-from selenium import webdriver
 
 sys.path.insert(0, os.path.dirname(__file__))
-driver = webdriver.Firefox()
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     message = 'Hello people!\n'
     version = 'Python %s\n' % sys.version.split()[0]
-    resp = driver.current_url
+    resp = os.environ['REQUEST_URI']
     response = '\n'.join([message, version, resp])
     return [response.encode()]
