9d35945aa7895eea256b7c90a2fa00862c0ace16
Apelsini <apelsin652@gmail.com>
2022-08-08 17:09:40 +0300

    sys.path insert check

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,12 @@
 import os
 import sys
+import requests
 
 sys.path.insert(0, os.path.dirname(__file__))
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    message = 'Hello people!\n'
+    message = 'Hello people!\n'+sys.path.__str__()
     version = 'Python %s\n' % sys.version.split()[0]
     response = '\n'.join([message, version])
     return [response.encode()]
