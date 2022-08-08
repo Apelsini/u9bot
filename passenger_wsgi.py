056bb9e1fb8a49d2ad188c083ad7409089968971
Apelsini <apelsin652@gmail.com>
2022-08-08 17:45:45 +0300

    chrome webdriver check

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,16 @@
 import os
 import sys
 import requests
+from selenium import webdriver
 
 sys.path.insert(0, os.path.dirname(__file__))
+driver = webdriver.Chrome()
+resp = driver.current_url
+driver.quit()
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
     message = 'Hello people!\n'
     version = 'Python %s\n' % sys.version.split()[0]
-    resp = os.environ['REQUEST_URI']
     response = '\n'.join([message, version, resp])
     return [response.encode()]
