8bc882cc3f8b786ff54f747f17572c993e517bcf
Apelsini <apelsin652@gmail.com>
2022-08-09 14:45:10 +0300

    bot view

--- bot/urls.py
+++ bot/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('bot', views.bot, name='bot'),
     path('c19/', views.c19, name='c19'),
 ]
--- bot/views.py
+++ bot/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def bot(request):
     return HttpResponse("Hello, world. You're at the bot index. Checking the change in the message")
 
 def c19(request):
