29dc9900797f797237cbb68015bfd96223ced5dc
Apelsini <apelsin652@gmail.com>
2022-08-09 12:17:13 +0300

    back to Hello people

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,13 +1,13 @@
-from u9robot.wsgi import application
-# import os
-# import sys
-#
-# sys.path.insert(0, os.path.dirname(__file__))
-#
-# def application(environ, start_response):
-#     start_response('200 OK', [('Content-Type', 'text/plain')])
-#     message = 'Hello people!\n'
-#     version = 'Python %s\n' % sys.version.split()[0]
-#     resp = '0'
-#     response = '\n'.join([message, version, resp])
-#     return [response.encode()]
+//from u9robot.wsgi import application
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(__file__))
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'Hello people!\n'
+    version = 'Python %s\n' % sys.version.split()[0]
+    resp = '0'
+    response = '\n'.join([message, version, resp])
+    return [response.encode()]
