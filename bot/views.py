1b979f7ffee07792faed6022654d987a47b24013
Apelsini <apelsin652@gmail.com>
2022-08-09 15:21:22 +0300

    data about vaccination

--- bot/views.py
+++ bot/views.py
@@ -6,10 +6,10 @@ import requests, json
 def index(request):
     return HttpResponse("Hello, world. You're at the bot index. Checking the change in the message")
 
-def c19(request):
+def c19(request): # this view gets vaccination rate about Belarus from Our world in data
     url = requests.get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json")
     text = url.text
     data = json.loads(text)
-    country = data[19]
-    return HttpResponse(country['country'])
+    country = data[19] #Belarus is no 19 in the Our World in Data list
+    return HttpResponse(country['data'])
 # Create your views here.
