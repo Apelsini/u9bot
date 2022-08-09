6f254ee9b94957695711a662ba2db044447f4efe
Apelsini <apelsin652@gmail.com>
2022-08-09 15:02:15 +0300

    vaccinations json

--- bot/views.py
+++ bot/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests, json
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the bot index. Checking the change in the message")
 
 def c19(request):
-    return HttpResponse("covid19 json will be here")
+    url = requests.get("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.json")
+    text = url.text
+    return HttpResponse(text)
 # Create your views here.
