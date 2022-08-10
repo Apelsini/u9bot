1bded13a952691a79e6d69a353c7871b1b68d85a
Apelsini <apelsin652@gmail.com>
2022-08-10 13:04:02 +0300

    c19cd urls

--- bot/urls.py
+++ bot/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('c19/', views.c19, name='c19'),
-    path('c19cd/', views.c19, name='c19cd'),
+    path('c19cd/', views.c19cd, name='c19cd'),
 ]
