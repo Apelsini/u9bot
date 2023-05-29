ad427b12d806c77982df8f7edde1a70360b9bd1b
Apelsini <apelsin652@gmail.com>
2023-05-29 17:32:20 +0300

    deploy

--- u9botpr/settings.py
+++ u9botpr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%s0nu*ofw1vn-*nf46nm16p+jasez$abqtekrn*!xir&csq+ab
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bot.u9.by', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['bot.u9.by', '127.0.0.1:8000', 'www.bot.u9.by']
 
 
 # Application definition
