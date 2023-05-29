e8f2aeb1a7a369efbb640bf42fe9f4ce39c1438c
Apelsini <apelsin652@gmail.com>
2023-05-29 15:50:57 +0300

    Update urls.py

--- u9botpr/urls.py
+++ u9botpr/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('bot.urls')),
     path('bot/', include('bot.urls')),
     path('admin/', admin.site.urls),
 ]
