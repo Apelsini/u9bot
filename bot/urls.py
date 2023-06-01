4fc4c41bb2b4bca649ac009ab7f548525af7b488
Apelsini <apelsin652@gmail.com>
2023-06-01 17:19:44 +0300

    1

--- bot/urls.py
+++ bot/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 from . import views
+from .views import create_view
 
 #https://www.geeksforgeeks.org/django-crud-create-retrieve-update-delete-function-based-views/
-
 
 urlpatterns = [
     path('', view=views.IndexView.as_view(), name='index'),
     path('bot/', view=views.IndexView.as_view(), name='index'),
-    path('add/', view=views.create_view(), name='add'),
+    path('add/', view=create_view, name='add'),
     path('c19/', views.c19, name='c19'),
     path('c19cd/', views.c19cd, name='c19cd'),
 ]
